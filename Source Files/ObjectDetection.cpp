d3632773cd8435d0978413ca2d32f7e878d87cc8
Reza <rakib08cse@gmail.com>
2021-07-21 07:48:35 +0200

    Update ObjectDetection.cpp

--- Source Files/ObjectDetection.cpp
+++ Source Files/ObjectDetection.cpp
@@ -13,13 +13,9 @@ using namespace std;
 
 int main()
 {
-	ObjDetection obj("E:/Backup Windows/code_repository/Visual Studio Projects/OpenCV Projects/ObjectDetection/testImage.PNG");
+	ObjDetection obj("your_path_to_test_image/testImage.PNG");
 	obj.loadNetwork();
 	obj.processImage();
-	/*Mat image = Mat::zeros(300, 600, CV_8UC3);
-	circle(image, Point(250, 150), 100, Scalar(0, 255, 128), -100);
-	circle(image, Point(350, 150), 100, Scalar(255, 255, 255), -100);
-	imshow("Display Window", image);*/
 	waitKey(0);
 	return 0;
 }
